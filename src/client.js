02c39c878b5be1731ce37c24bf88b7c1967cbfba
Andy Eblin <andy@wearemage.com>
2023-05-03 12:09:09 -0700

    Update client.js

--- src/client.js
+++ src/client.js
@@ -1,11 +1,19 @@
-import got from 'got'
-import { COLLECTION_QUERY } from './queries'
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var got = require('got');
+var queries = require('./queries.js');
+
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var got__default = /*#__PURE__*/_interopDefaultLegacy(got);
 
 /**
  * Create a Shopify Storefront GraphQL client for the provided name and token.
  */
-export const createClient = ({ storeUrl, storefrontToken, timeout }) => got.extend({
-  prefixUrl: `${storeUrl}/api/2022-01`,
+const createClient = ({ storeUrl, storefrontToken, timeout }) => got__default['default'].extend({
+  prefixUrl: `${storeUrl}/api/2023-04`,
   headers: {
     'X-Shopify-Storefront-Access-Token': storefrontToken
   },
@@ -13,13 +21,13 @@ export const createClient = ({ storeUrl, storefrontToken, timeout }) => got.exte
   responseType: 'json',
   retry: 2,
   timeout
-})
+});
 
 /**
  * Get all paginated data from a query. Will execute multiple requests as
  * needed.
  */
-export const queryAll = async (client, query, variables) => {
+const queryAll = async (client, query, variables) => {
   const items = client.paginate.each('graphql.json', {
     method: 'POST',
     json: { query, variables },
@@ -30,41 +38,41 @@ export const queryAll = async (client, query, variables) => {
         return data.data.edges
       },
       paginate: (response, allItems, currentItems) => {
-        const { errors, data } = response.body
+        const { errors, data } = response.body;
         if (errors) throw new Error(errors[ 0 ].message)
 
-        const { pageInfo } = data.data
+        const { pageInfo } = data.data;
         if (!pageInfo.hasNextPage) return false
 
-        const lastItem = currentItems[ currentItems.length - 1 ]
-        const newVariables = { ...variables, after: lastItem.cursor }
+        const lastItem = currentItems[ currentItems.length - 1 ];
+        const newVariables = { ...variables, after: lastItem.cursor };
 
         return {
           json: { query, variables: newVariables }
         }
       }
     }
-  })
+  });
 
-  const allNodes = []
+  const allNodes = [];
   for await (const { node, typeName } of items) {
     if (typeName !== 'CollectionEdge') {
-      allNodes.push(node)
+      allNodes.push(node);
       continue
     }
 
     // Currently setup for Collection.products field, but can extend this method in future, if needed
     if (!node.products.pageInfo.hasNextPage) {
-      allNodes.push(node)
+      allNodes.push(node);
       continue
     }
 
-    const lastProduct = node.products.edges[ node.products.edges.length - 1 ]
-    const collectionVariables = { ...variables, handle: node.handle, after: lastProduct.cursor }
+    const lastProduct = node.products.edges[ node.products.edges.length - 1 ];
+    const collectionVariables = { ...variables, handle: node.handle, after: lastProduct.cursor };
 
     const remainingProducts = await client.paginate.all('graphql.json', {
       method: 'POST',
-      json: { query: COLLECTION_QUERY, variables: collectionVariables },
+      json: { query: queries.COLLECTION_QUERY, variables: collectionVariables },
       pagination: {
         backoff: 1000,
         transform: ({ body: { data, errors } }) => {
@@ -72,25 +80,28 @@ export const queryAll = async (client, query, variables) => {
           return data.collection.products.edges
         },
         paginate: (response, allItems, currentItems) => {
-          const { errors, data } = response.body
+          const { errors, data } = response.body;
           if (errors) throw new Error(errors[ 0 ].message)
 
-          const { pageInfo } = data.collection.products
+          const { pageInfo } = data.collection.products;
           if (!pageInfo.hasNextPage) return false
 
-          const lastItem = currentItems[ currentItems.length - 1 ]
-          const newVariables = { ...collectionVariables, after: lastItem.cursor }
+          const lastItem = currentItems[ currentItems.length - 1 ];
+          const newVariables = { ...collectionVariables, after: lastItem.cursor };
 
           return {
-            json: { query: COLLECTION_QUERY, variables: newVariables }
+            json: { query: queries.COLLECTION_QUERY, variables: newVariables }
           }
         }
       }
-    })
+    });
 
-    const edges = [...node.products.edges, ...remainingProducts]
-    allNodes.push({ ...node, products: { edges } })
+    const edges = [...node.products.edges, ...remainingProducts];
+    allNodes.push({ ...node, products: { edges } });
   }
 
   return allNodes
-}
+};
+
+exports.createClient = createClient;
+exports.queryAll = queryAll;
